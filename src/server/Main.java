683f860b1e213d1cd09acbd9a45f9799d1dceda3
detemegandy <andreas.saltveit@gmail.com>
2014-02-14 00:47:12 +0100

    server stop?
    never stop, metanet to important

--- src/server/Main.java
+++ src/server/Main.java
@@ -3,10 +3,13 @@ package server;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
 		//new HTMLpage();
 		TestServer server = new TestServer();
 		server.start();
 		server.join();
+		/*
+		 * burde kanskje legge inn vent 10 min så:
+		 * server.stop();
+		 */
 	}
 }
