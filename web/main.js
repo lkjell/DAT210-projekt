3f63ab06a66498cde66ac0b40b6b984ef6fd527b
Deagels <joakim.hagen@lyse.net>
2014-02-13 22:28:16 +0100

    Sortable image boxes

---
+++ web/main.js
@@ -0,0 +1,9 @@
+$( function() {
+	//$( ".image" ).draggable({
+	//	containment:       ".images",
+	//	connectToSortable: ".image",
+	//	scroll:            true,
+	//	revert:            true
+	//});
+	$( ".images" ).sortable().disableSelection();
+});
