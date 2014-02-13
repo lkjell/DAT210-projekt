eaabc494a215d268f53c81d17a5302b4087efe01
fossit <jonatanpettersen@gmail.com>
2014-02-13 14:49:53 +0100

    initial commit
    Added base class for Image Servlet

---
+++ src/server/ImageServlet.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author jonatanp jonatanpettersen@gmail.com
+ * 
+ * This class listens for Image Requests and returns Image 
+ */
+public class ImageServlet {
+
+}
