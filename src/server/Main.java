789db7aa662a4fee68e4b0b17e5a0ec228a9972b
detemegandy <andreas.saltveit@gmail.com>
2014-02-16 02:55:03 +0100

    Revert "a"
    This reverts commit e598e48f4b419f43a554783ace36ca0189994a5a.

--- src/server/Main.java
+++ src/server/Main.java
@@ -5,14 +5,17 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		//new HTMLpage();
 		TestServer server = new TestServer();
+<<<<<<< HEAD
 //		int timeToWait = 10*1000; // x*1000 ms
 //		server.wait(timeToWait);
 //		System.out.println("Server up for " + timeToWait + ", stopping...");
 //		server.stop();
 //		System.out.println("Server has stopped.");
+=======
 		server.start();
 		server.join();
 		System.out.println( "Continued main thread. Exiting gracefully." );
 		System.exit(0); // exit gracefully
+>>>>>>> 3bfade2dae7bd2471c0631ffdc7bdbe1490f82d4
 	}
 }
--- src/server/TestServer.java
+++ src/server/TestServer.java
@@ -6,7 +6,6 @@ package server;
 
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.DefaultHandler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
@@ -15,27 +14,30 @@ public class TestServer extends Server {
 	
 	public TestServer() {
 		super(8080);
-//        ResourceHandler resource_handler = new ResourceHandler();
-//        resource_handler.setDirectoriesListed(true);
-//        resource_handler.setWelcomeFiles(new String[] { "/web/index.html" });
-//        resource_handler.setResourceBase(".");
-//        HandlerList handlers = new HandlerList();
-//        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
-//        super.setHandler(handlers);
-//        try {
-//			super.start();
-//	        super.join();
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		ContextHandler context = new ContextHandler();
-        context.setContextPath("../web");
-        context.setResourceBase(".");
-        context.setClassLoader(Thread.currentThread().getContextClassLoader());
-        context.setHandler(new TestHandler());
-		this.setHandler(context);
+<<<<<<< HEAD
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[] { "/web/index.html" });
+        resource_handler.setResourceBase(".");
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+        super.setHandler(handlers);
+        try {
+			super.start();
+	        super.join();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+=======
+//		ContextHandler context = new ContextHandler();
+//        context.setContextPath("../web");
+//        context.setResourceBase(".");
+//        context.setClassLoader(Thread.currentThread().getContextClassLoader());
+//        context.setHandler(new TestHandler());
+//		this.setHandler(context);
 		this.setHandler(new TestHandler());
 		ServerTrayIcon.make(this);
+>>>>>>> 3bfade2dae7bd2471c0631ffdc7bdbe1490f82d4
 	}
 }
