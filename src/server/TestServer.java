a03696eb0ecdd0ca67fae75a86564413941c4d47
detemegandy <andreas.saltveit@gmail.com>
2014-02-16 03:55:54 +0100

    images show up ^^

--- src/server/TestServer.java
+++ src/server/TestServer.java
@@ -4,27 +4,30 @@
 
 package server;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
 
-public class TestServer extends Server {
-	
-	private int port;
-	
-	public TestServer() { this( 80 ); }
-	public TestServer( int port ) {
-		super( port );
-//		ContextHandler context = new ContextHandler();
-//        context.setContextPath("../web");
-//        context.setResourceBase(".");
-//        context.setClassLoader(Thread.currentThread().getContextClassLoader());
-//        context.setHandler(new TestHandler());
-//		this.setHandler(context);
-		this.setHandler( new TestHandler() );
-		ServerTrayIcon.make( this );
-		this.port = port;
-	}
-	
-	public void test() {}
-	
-	public int getPort() { return port;	}
-}
+ 	public class TestServer extends Server {
+ 		
+ 		public TestServer(int portN) {
+ 			super(portN);
+			this.setHandler(new TestHandler());
+	        ResourceHandler resource_handler = new ResourceHandler();
+	        resource_handler.setDirectoriesListed(true);
+	        resource_handler.setWelcomeFiles(new String[] { "/web/index.html" });
+	        resource_handler.setResourceBase(".");
+	        HandlerList handlers = new HandlerList();
+	        handlers.setHandlers(new Handler[] { resource_handler, new DefaultHandler() });
+	        super.setHandler(handlers);
+	        try {
+				super.start();
+		        super.join();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+ 		}
+ 	}
