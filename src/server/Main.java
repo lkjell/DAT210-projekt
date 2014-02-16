9f66c2a3f3787154e039c0026d4482b8dc63b38e
detemegandy <andreas.saltveit@gmail.com>
2014-02-16 01:54:26 +0100

    bildene komme ihvertfall opp ;)
    beeing drunk is excellent for server configuration ;)

--- src/server/Main.java
+++ src/server/Main.java
@@ -5,11 +5,10 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		//new HTMLpage();
 		TestServer server = new TestServer();
-		server.start();
-		server.join();
-		/*
-		 * burde kanskje legge inn vent 10 min så:
-		 * server.stop();
-		 */
+//		int timeToWait = 10*1000; // x*1000 ms
+//		server.wait(timeToWait);
+//		System.out.println("Server up for " + timeToWait + ", stopping...");
+//		server.stop();
+//		System.out.println("Server has stopped.");
 	}
 }
