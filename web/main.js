4839f3ee4bc4410086186a834a4bfad67404f175
Deagels <joakim.hagen@lyse.net>
2014-02-18 00:48:16 +0100

    javascript mouseover bilde til front

--- web/main.js
+++ web/main.js
@@ -6,4 +6,8 @@ $( function() {
 	//	revert:            true
 	//});
 	$( ".images" ).sortable().disableSelection();
+	$( ".image" ).hover(
+		function() { this.style.zIndex = '2'; }, // mouse enter
+		function() { this.style.zIndex = '1'; }  // mouse leave
+	);
 });
