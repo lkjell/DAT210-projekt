3556a6d37b6071845b34163babaf6720eda94197
Kjell Le <lkjell@gmail.com>
2014-02-18 13:19:05 +0100

    Send a list of all filename in img/ when request all filenames.

--- src/server/RequestFilename.java
+++ src/server/RequestFilename.java
@@ -1,5 +1,6 @@
 package server;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -14,12 +15,40 @@ public class RequestFilename extends AbstractHandler
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 	throws IOException, ServletException {
 
-		if ("all".equals(request.getParameter("filename"))) {
+		String parameter = request.getParameter("filename");
+
+		File dir = new File("img/");
+		String[] list_of_files = dir.list();
+
+		if ("all".equals(parameter)) {
 			baseRequest.setHandled(true);
 			response.setContentType("text/html;charset=utf-8");
 			response.setStatus(HttpServletResponse.SC_OK);
 
-			response.getWriter().println("Send all filename");
+			response.getWriter().println("Send all filename<br>");
+
+			for(String index:list_of_files) {
+				response.getWriter().println(index + "<br>");
+			}
+		} else {
+
+			try {
+				int index = Integer.parseInt(parameter);
+
+				baseRequest.setHandled(true);
+				response.setContentType("text/html;charset=utf-8");
+				response.setStatus(HttpServletResponse.SC_OK);
+
+				response.getWriter().println("Send file: " + index);
+
+			} catch(Exception e){
+				e.printStackTrace();
+				baseRequest.setHandled(true);
+				response.setContentType("text/html;charset=utf-8");
+				response.setStatus(HttpServletResponse.SC_OK);
+
+				response.getWriter().println("Invalid file index");
+			}
 		}
 	}
 }
