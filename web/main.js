121914a2b4578415099ff7fe8eb25f2cb66d67c0
aumym <aumym@live.com>
2014-03-02 22:18:59 +0100

    Fullscreen
    fullscreen på serveren yes

--- web/main.js
+++ web/main.js
@@ -1,13 +1,43 @@
 $( function() {
-	//$( ".image" ).draggable({
-	//	containment:       ".images",
-	//	connectToSortable: ".image",
-	//	scroll:            true,
-	//	revert:            true
-	//});
+$('body').append('<div id="largeImgPanel" onclick="hideMe(this);"><img id="largeImg" style="height: 100%; margin: 0; padding: 0;" /></div>');
+});
+
+$( function() {
+	$( ".image" ).draggable({
+		containment:       ".images",
+		connectToSortable: ".image",
+		scroll:            true,
+		revert:            true
+	});
 	$( ".images" ).sortable().disableSelection();
 	$( ".image" ).hover(
 		function() { this.style.zIndex = '2'; }, // mouse enter
 		function() { this.style.zIndex = '1'; }  // mouse leave
 	);
+	
+	$( ".image" ).click(
+		function () {
+			var imgSource = $(this).find('img').attr('src');
+			document.getElementById('largeImg').src = imgSource; 
+			showLargeImagePanel();
+			unselectAll();
+			//alert(imgSource);
+		}
+		
+	);
+	
 });
+
+//alert("test") kontrol + q for comment
+
+            function showLargeImagePanel() {
+            document.getElementById('largeImgPanel').style.visibility = 'visible';
+			
+            }
+            function unselectAll() {
+                if(document.selection) document.selection.empty();
+                if(window.getSelection) window.getSelection().removeAllRanges();
+            }
+            function hideMe(obj) {
+                obj.style.visibility = 'hidden';
+            }
