34c7dec96a5a226ad871646fe0dac5c672d0651d
Deagels <joakim.hagen@lyse.net>
2014-03-04 18:06:50 +0100

    fullview. dobbelklikk på bilde i gridview.

--- web/main.js
+++ web/main.js
@@ -1,17 +1,20 @@
 $( function() {
-	//$( ".image" ).draggable({
-	//	containment:       ".images",
-	//	connectToSortable: ".image",
-	//	scroll:            true,
-	//	revert:            true
-	//});
+
 	$( ".images" ).sortable().disableSelection();
+
 	$( ".image" ).hover(
 		function() { this.style.zIndex = '2'; }, // mouse enter
 		function() { this.style.zIndex = '1'; }  // mouse leave
 	);
+
 	$( ".image" ).click(function(){
 		requestMetadata($( this ).find(">").attr( "src" ));
+	});
+
+	$( ".image" ).dblclick( showLargeImagePanel );
+	
+	$( ".largeImgPanel" ).dblclick( function() {
+		$('.largeImgPanel').css( 'visibility', 'hidden' );
 	});
 });
 
@@ -30,4 +33,13 @@ function requestMetadata( file ) {
 			alert( status +"\n"+ error );
 		}
 	});
+}
+
+function showLargeImagePanel() {
+	var imgSource = $( this ).find('img').attr('src');
+	console.log(imgSource);
+	$('#largeImg').attr( 'src', imgSource );
+	$('.largeImgPanel').css( 'visibility', 'visible');
+	if(document.selection) document.selection.empty();
+	if(window.getSelection) window.getSelection().removeAllRanges();
 }
