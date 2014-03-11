7fd2d5fac48649354eab48e6c0a151aa46801f83
Diroy <diroys@hotmail.com>
2014-03-11 14:57:35 +0100

    Knapper

--- web/main.js
+++ web/main.js
@@ -15,35 +15,51 @@ $( function() {
 		function() { this.style.zIndex = '1'; }  // mouse leave
 	);
 	
-	$( ".image" ).click(
-		function () {
+	$( ".image" ).click(function () {
 			var imgSource = $(this).find('img').attr('src');
 			document.getElementById('largeImg').src = imgSource; 
 			showLargeImagePanel();
+			$('#button').css('hidden');
+			skjulKnappene();
 			unselectAll();
-
-			//alert(imgSource);
-		}
-		
-	);
+			$(".image").keypress();
+	});
 	
 });
-document.getElementById('button').style.visibility='hidden';
 
-            function skjulKnappene() {
-            	document.getElementById('button').style.visibility='hidden';
-            }
+function skjulKnappene() {
+	$('#button').css('hidden');
+}
 
-            function showLargeImagePanel() {
-            	document.getElementById('largeImgPanel').style.visibility = 'visible';
-            	document.getElementById('button').style.visibility='visible';
-            }
+function visKnappene() {
+	$('#button').css('visibility');
+}
 
-            function unselectAll() {
-                if(document.selection) document.selection.empty();
-                if(window.getSelection) window.getSelection().removeAllRanges();
-            }
+function showLargeImagePanel() {
+	document.getElementById('largeImgPanel').style.visibility = 'visible';
+	$('#button').css('hidden');
+}
 
-            function hideMe(obj) {
-                obj.style.visibility = 'hidden';
-            }
+function unselectAll() {
+    if(document.selection) document.selection.empty();
+    if(window.getSelection) window.getSelection().removeAllRanges();
+}
+
+function hideMe(obj) {
+    obj.style.visibility = 'hidden';
+}
+
+
+//Disabler scrollbar
+//var ar=new Array(33,34,35,36,37,38,39,40);
+
+//$(document).keydown(function(e) {
+     //var key = e.which;
+      //console.log(key);
+      //if(key==35 || key == 36 || key == 37 || key == 39)
+      //if($.inArray(key,ar) > -1) {
+          //e.preventDefault();
+          //return false;
+      //}
+      //return true;
+//});
