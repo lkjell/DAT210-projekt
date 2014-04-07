a36fb5158194972bb6814d893e890cf596da27bd
asbtho <validemail@hotmail.com>
2014-04-07 15:33:44 +0200

    Working fileuploadhandler
    see http://commons.apache.org/proper/commons-fileupload/using.html

---
+++ src/server/FileUploadHandler.java
@@ -0,0 +1,97 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class FileUploadHandler extends AbstractHandler {
+	
+	private String savePath = "c:\\";
+	private String tempFolder = "c:\\temp";
+	private File file ;
+        
+	@Override
+	public void handle(String target, Request baseRequest, HttpServletRequest request,HttpServletResponse response) 
+			throws IOException, ServletException {
+		
+		java.io.PrintWriter out = response.getWriter( );
+		
+        if(request.toString().contains("POST")){
+        	System.out.println("Request is a POST request");
+        	
+        	if (request.getContentType() != null && request.getContentType().startsWith("multipart/form-data")) {
+        		System.out.println("ContentType is MultiPart");
+        		
+        		// Check that we have a file upload request
+        		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        		if( isMultipart) System.out.println("File is multipart");
+        		
+        		// Create a factory for disk-based file items
+        		DiskFileItemFactory factory = new DiskFileItemFactory();
+
+        		// Configure a repository (to ensure a secure temp location is used)
+        		factory.setRepository(new File(tempFolder));
+
+        		// Create a new file upload handler
+        		ServletFileUpload upload = new ServletFileUpload(factory);
+
+        		try{ 
+        			// Parse the request to get file items.
+        			List fileItems = upload.parseRequest(request);
+    			
+        			// Process the uploaded file items
+        			Iterator i = fileItems.iterator();
+
+        			out.println("<html>");
+        			out.println("<head>");
+        			out.println("<title>Servlet upload</title>");  
+        			out.println("</head>");
+        			out.println("<body>");
+        			
+        			while ( i.hasNext () ){
+        				FileItem fi = (FileItem)i.next();
+        				if ( !fi.isFormField () ){
+	    		            // Get the uploaded file parameters
+	    		            String fieldName = fi.getFieldName();
+	    		            String fileName = fi.getName();
+	    		            String contentType = fi.getContentType();
+	    		            boolean isInMemory = fi.isInMemory();
+	    		            long sizeInBytes = fi.getSize();
+	    		            // Write the file
+	    		            if( fileName.lastIndexOf("\\") >= 0 ){
+	    		               file = new File( savePath + 
+	    		               fileName.substring( fileName.lastIndexOf("\\"))) ;
+	    		            }else{
+	    		               file = new File( savePath + 
+	    		               fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+	    		            }
+	    		            fi.write( file ) ;
+	    		            out.println("Uploaded Filename: " + fileName + "<br>");
+        				}
+        			}
+        			out.println("</body>");
+        			out.println("</html>");
+        		}catch(Exception ex) {
+        			System.out.println(ex);
+        		}
+        	}
+        	
+		}
+        
+		System.out.println(request.toString());
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
