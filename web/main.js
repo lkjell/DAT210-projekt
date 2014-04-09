ffa48c406c85a71653770a19b2f6dc01f5309f10
Diroy <diroys@hotmail.com>
2014-04-09 21:40:04 +0200

    FixedFullview

--- web/main.js
+++ web/main.js
@@ -1,3 +1,6 @@
+// $( function() {
+// $('body').append('<div id="largeImgPanel" onclick="hideMe(this);"><img id="largeImg" style="height: 100%; margin: 0; padding: 0;" /></div>');
+// });
 
 $( function() {
 	$( ".image" ).draggable({
@@ -12,82 +15,62 @@ $( function() {
 		function() { this.style.zIndex = '1'; }  // mouse leave
 	);
 	
-	$( ".image" ).click(function () {
+	$( ".image" ).click(
+		function () {
 			var imgSource = $(this).find('img').attr('src');
 			document.getElementById('largeImg').src = imgSource; 
 			showLargeImagePanel();
-			//$('#button').css('hidden');
-			//skjulKnappene();
-			//unselectAll();
-			//$(".image").keypress();
-	});
+			unselectAll();
+			//alert(imgSource);
+		}
+	);
+
+	/*$(document).keydown(function(e){
+    	if (e.keyCode == 37) { 
+       		alert( "left pressed " );
+       		return false;
+    	}
+    	if (e.keyCode == 39) { 
+       		alert( "right pressed " );
+       		return false;
+    	}
+	});*/
 	
-		$( ".largeImgPanel" ).click(
+	$( ".largeImgPanel" ).click(
 		function() {
 		document.getElementById('largeImgPanel').style.visibility = 'hidden';
 		});
 
+
 	
 });
 
-//vic
+//alert("test") kontrol + q for comment
+
+			// function hideLargeImagePanel() {
+            // document.getElementById('largeImgPanel').style.visibility = 'hidden';
+            // }
+
             function showLargeImagePanel() {
             document.getElementById('largeImgPanel').style.visibility = 'visible';
 			
             }
-            //function unselectAll() {
+            function unselectAll() {
                 if(document.selection) document.selection.empty();
                 if(window.getSelection) window.getSelection().removeAllRanges();
             }
 
-
-//diroy
-function skjulKnappene() {
-	$('#button').css('hidden');
+imgs=Array(1,2,3,4,5);
+var i=0;
+document.onkeydown = checkKey;
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '37') {
+        document.getElementById(".image").src=imgs[--i];
+    }
+    else if (e.keyCode == '39') {
+        document.getElementById(".image").src=imgs[++i];
+    }
 }
 
-function visKnappene() {
-	$('#button').css('visibility');
-}
-
-function showLargeImagePanel() {
-	document.getElementById('largeImgPanel').style.visibility = 'visible';
-	$('#button').css('hidden');
-}
-
-//function unselectAll() {
-    if(document.selection) document.selection.empty();
-    if(window.getSelection) window.getSelection().removeAllRanges();
-}
-
-//function hideMe(obj) {
-    obj.style.visibility = 'hidden';
-}
-
-<script type="text/javascript">
-/* KEYNAV */
-document.onkeydown = function(e) {
-if (! e) var e = window.event;
-var code = e.charCode ? e.charCode : e.keyCode;
-if (! e.shiftKey && ! e.ctrlKey && ! e.altKey && ! e.metaKey) {
-if (code == Event.KEY_LEFT) {
-if ($('previous_page_link')) location.href = $('previous_page_link').href;
-} else if (code == Event.KEY_RIGHT) {
-if ($('next_page_link')) location.href = $('next_page_link').href;}
-}
-}); 
-</script>
-
-//Disabler scrollbar
-//var ar=new Array(33,34,35,36,37,38,39,40);
-
-//$(document).keydown(function(e) {
-     //var key = e.which;
-      //console.log(key);
-      //if(key==35 || key == 36 || key == 37 || key == 39)
-      //if($.inArray(key,ar) > -1) {
-          //e.preventDefault();
-          //return false;
-      //}
-      //return true;
-//});
+//src ==> getElementById
