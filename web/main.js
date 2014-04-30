41ba4ae5286a8041eb0bea62e0b3f48accaa8037
Deagels <joakim.hagen@lyse.net>
2014-04-30 21:50:39 +0200

    Removed legacy code

--- web/main.js
+++ web/main.js
@@ -224,8 +224,10 @@ function buildGrid( ids ) {
 	for( var i=0; i<ids.length; i++ ) {
 		if( imageById[ids[i]] == undefined ) new Image( ids[i] );
 		images.append(
-			$( '<li>' ).attr( 'class', "image" ).attr( 'id', ids[i] ).append(
-			$( '<img>' ).attr( 'src', "img/?img_id="+ ids[i] ))
+			$( '<li>' ).attr( 'class', "image" )
+			.attr( 'id', ids[i] ).append(
+				$( '<img>' ).attr( 'src', "img/?img_id="+ ids[i] )
+			)
 		);
 	}
 	$( '.image' ).hover(
@@ -281,74 +283,6 @@ function updateSidebar( img_id ) {
 			$( '<p>' ).text( "keywords: "), kwsfelt
 		);
 		$( ".right" ).html( container );
-	}
-}
-
-function updateKeyword( oldKw, newKw ) {
-	post( "meta/?old="+ oldKw +"&new="+ newKw, function() {
-		alert( oldKw +" changed to "+ newKw );
-	});
-}
-
-function requestMetadata( file_id ) {
-	console.log("ajax request "+ file_id);
-	$.ajax({
-		url: "getTags?img_id="+ file_id,
-		dataType: 'json',
-		success: function( data, status, xhr ) {
-			dom = $( '<div>' );
-			recursive( dom, data );
-			dom.accordion({
-				heightStyle: "content"
-			});
-			/*$.map(data[0], function(value, key) {
-				h3 = $( '<h3>' ).text( key );
-				string = "";
-				for ( var i=0; i < value.length; i++ ) {
-					string += value;
-				}
-				sub = $('<div>').text( string );
-				console.log( $('<div>').append(sub).html() );
-				//text += key + ": "
-				accordion.append( h3, sub );
-			});
-
-			$( ".right" ).append( accordion );*/
-		},
-		error: function( xhr, status, error ) {
-			alert( status +"\n"+ error );
-		}
-	});
-
-	function recursive(dom, data) {
-
-		$.map(data, function( value, key ) {
-					console.log( "key; " + key );
-			h3 = $( '<h3>' ).text( key );
-			sub = $('<div>');
-			for ( var i = 0; i < value.length; i++ ) {
-				if( i === 0 ){
-					if ( value[i] === null ) { continue;	}
-					ul = $('<ul>')
-					for(i=0;i<value.length;i++){
-						//console.log( faen + ":" + helvette );
-						ul.append( $('<li>').text( value[i] ));
-						console.log(ul.html())
-					};
-					//console.log( $('<div>').append(ul).html() );
-					sub.html( ul );
-					console.log( $('<div>').append(sub).html() );
-					break;
-				}
-				recursive(dom, value[i]);
-			}
-			
-			//console.log( $('<div>').append(sub).html() );
-			//text += key + ": "
-			dom.append( h3, sub );
-		});
-		console.log("fuck you");
-		$( ".right" ).html( dom );
 	}
 }
 
